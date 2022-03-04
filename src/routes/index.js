15b4a9d23b2aeebfa927abfaa39ae6c42d2032f2
Brian Clark <clarkio@users.noreply.github.com>
2022-03-04 21:45:04 +0000

    fix: add csrf protection options

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const csrf = require('csurf');
 const router = express.Router();
 
+const csrfProtection = csrf({ cookie: true });
+
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', csrfProtection, function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', csrfProtection, function (req, res, next) {
+  res.send({ message: 'Success' });
 });
 
 module.exports = router;
