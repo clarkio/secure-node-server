ce80011251f07631e095f594645499c078277553
Brian Clark <clarkio@users.noreply.github.com>
2022-03-04 21:48:21 +0000

    fix: update csrf approach

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const csrf = require('csurf');
 
 const indexRouter = require('./routes/index');
 
@@ -11,6 +12,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(csrf({ cookie: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const csrf = require('csurf');
 const router = express.Router();
 
-const csrfProtection = csrf({ cookie: true });
-
 /* GET home page. */
-router.get('/', csrfProtection, function (req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', csrfProtection, function (req, res, next) {
+router.post('/', function (req, res, next) {
   res.send({ message: 'Success' });
 });
 
