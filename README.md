e38ef164c936ad3c959325bd34279abfc25caba0
Brian Clark <clarkio@users.noreply.github.com>
2022-03-04 21:26:04 +0000

    fix: use https
    add: script to create self-signed certificate for local development

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # secure-node-server
 A Node.js server template that's secure by default and comes with some simple boilerplate setup.
+
+
+## Getting Started
+
+1. `npm install`
+1. Generate a self-signed certificate using the script found in `certificate/create-dev-certificate`. Note: you may need to give it executable permissions in order to run it.
+1. `npm start`
--- src/server.js
+++ src/server.js
@@ -2,22 +2,33 @@
  * Module dependencies.
  */
 
-var app = require('./app');
-var debug = require('debug')('secure-node-server:server');
-var http = require('http');
+const app = require('./app');
+const debug = require('debug')('secure-node-server:server');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+
+/**
+ * Set up certificate options for the server
+ * Note: use this for local development but change for production
+ */
+const certOptions = {
+  key: fs.readFileSync(path.join(__dirname, '../key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, '../cert.pem')),
+};
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+const server = https.createServer(certOptions, app);
 
 /**
  * Listen on provided port, on all network interfaces.
@@ -32,7 +43,7 @@ server.on('listening', onListening);
  */
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
@@ -56,7 +67,7 @@ function onError(error) {
     throw error;
   }
 
-  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
@@ -78,7 +89,7 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
   debug('Listening on ' + bind);
 }
